55b8e8adffd55093b5c5d8fe41d01e71618c44dc
Raza Poonja <razapoonja@gmail.com>
2019-10-07 00:48:48 +0500

    Added functionality

--- src/App.js
+++ src/App.js
@@ -3,16 +3,78 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      newItem: '',
+      list: []
+    }
+  }
+
+  addItem(todoValue) {
+    if (todoValue !== '') {
+      const newItem = {
+        id: Date.now(),
+        value: todoValue,
+        isDone: false
+      };
+
+      const list = [...this.state.list];
+      list.push(newItem);
+
+      this.setState({
+        list,
+        newItem: ''
+      });
+    }
+  }
+
+  deleteItem(id) {
+    const list = [...this.state.list];
+    const updatedList = list.filter(item => item.id !== id);
+
+    this.setState({list: updatedList});
+  }
+
+  updateInput(input) {
+    this.setState({newItem: input});
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>React Todo App</h2>
+          <br />
+          <input 
+            type="text" 
+            placeholder="Write a Todo" 
+            required 
+            value={this.state.newItem}
+            onChange={e => this.updateInput(e.target.value)} />
+
+          <button 
+            onClick={() => this.addItem(this.state.newItem)}
+            disabled={!this.state.newItem.length}
+          >Add Todo</button>
+
+          <div>
+            <ul>
+              {this.state.list.map(item => {
+                return (
+                  <li key={item.id}>
+                    <input type="checkbox" checked={item.isDone} />
+                    {item.value}
+                    <button
+                      onClick={() => this.deleteItem(item.id)}
+                    >Delete</button>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
